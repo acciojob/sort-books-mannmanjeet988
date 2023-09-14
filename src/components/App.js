081837c9f88524dda64a0a2083838b1c4bee89c1
mannmanjeet988 <mannmanjeet988@gmail.com>
2023-09-14 20:23:05 +0530

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import BooksList from "./BooksList";
 const App = () => {
   return (
     <div>
-      <h1>SORT BOOKS</h1>
+      
       <BooksList />
         {/* Do not remove the main div */}
     </div>
